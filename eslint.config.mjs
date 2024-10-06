d3267dfb84abd45f838f15b142adef745f3176a2
B-S-B-Rabbit <mystery.knight113@gmail.com>
2024-10-06 19:58:51 +0300

    setting up eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(antfu({}), {
+  rules: {
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'always',
+          normal: 'always',
+          component: 'always',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+  },
+})
