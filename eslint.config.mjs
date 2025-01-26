b22d417471e034af247e3a28ad86829f6df89b37
B-S-B-Rabbit <mystery.knight113@gmail.com>
2025-01-26 16:14:33 +0300

    feature/GEOGREEN-8 #comment disable prettier lint warn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,7 +20,7 @@ export default withNuxt(antfu({}), eslintPluginPrettierRecommended, {
       },
     ],
     "prettier/prettier": [
-      "warn",
+      "off",
       {
         endOfLine: "auto",
       },
